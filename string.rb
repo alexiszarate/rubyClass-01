50d2fe1fc90f1448fc469407dc9b574a8fbb2210
alexiszarate <alexiszarate00@gmail.com>
2022-07-26 01:19:40 -0300

    first commit

---
+++ string.rb
@@ -0,0 +1,13 @@
+string = "My first string"
+
+puts  string.length
+
+puts string.count('i')
+
+puts string.slice(3, 5)
+
+puts string.upcase!
+
+puts string.empty? # cadena estas vacia?
+
+puts string.include? 'MY'
